55a9814305b6ddf6b33db592001bfeb8f688cb51
bdwain <bryandwain@gmail.com>
2014-09-25 00:00:42 -0500

    switch to rails api. still works

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,8 @@
-class ApplicationController < ActionController::Base
-  # Prevent CSRF attacks by raising an exception.
-  # For APIs, you may want to use :null_session instead.
-  protect_from_forgery with: :exception
+class ApplicationController < ActionController::API
+  #these includes are needed because ActionController::API doesn't include things devise needs
+  include ActionController::MimeResponds
+  include ActionController::ImplicitRender
+  include SimpleTokenAuthentication::ActsAsTokenAuthenticationHandler
 
   before_filter :configure_permitted_parameters, if: :devise_controller?
 
